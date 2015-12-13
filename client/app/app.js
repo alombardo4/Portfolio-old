36c8190b4efa5e8c9f852999cbbd7cde68479099
Alec Lombardo <alec.lombardo@gmail.com>
2015-12-13 18:44:51 -0500

    Initial commit. Generated from yo angular fullstack.

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('portfolioApp', [
+  'portfolioApp.auth',
+  'portfolioApp.admin',
+  'portfolioApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
